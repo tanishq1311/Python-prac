dc21ab4cf6f47352a6eb72d3834265bfa7d53627
Ubuntu <ubuntu@ip-172-31-4-91.ap-south-1.compute.internal>
2020-05-30 08:08:53 +0000

    Do foldering

---
+++ list/list_remove.py
@@ -0,0 +1,8 @@
+a = [1,2,3,4,5,6,7,8,9,0]
+print(a)
+a.pop(1)
+print(a)
+a.remove(3)
+print(a)
+a.clear()
+print(a)
---
+++ list/replace_element_list.py
@@ -0,0 +1,13 @@
+list1 = [1, 2, 3, 4, 5, 6,]
+
+# code
+list1[4] = 'swastik'
+list1[0] = 'tanishq'
+
+
+# testing
+if list1[0] == 'Tanishq':
+    print('Your code is correct. ')
+
+else:
+    print('Your code is useless')

480744745c65a5a00992b1200a18f37f7bd56018
Ubuntu <ubuntu@ip-172-31-5-11.ap-south-1.compute.internal>
2020-08-17 03:51:42 +0000

    Do backup

---
+++ def.py
@@ -0,0 +1,34 @@
+def my_function(child1, child2, child3, child4, mummy, papa, dadi):
+    print("this is youngest child in family: " + child2)
+
+my_function(child1 = "tanishq", child2 = "daksh", child3 = "diwakar", child4 = "swastik", mummy = "sangeeta", papa = "pankaj", dadi = "sulochna")
+
+def my_function(child1, child2, child3, child4, mummy, papa, dadi):
+    print("this is my cousins: " + child2, child3)
+
+my_function(child1 = "tanishq", child2 = "daksh", child3 = "diwakar", child4 = "swastik", mummy = "sangeeta", papa = "pankaj", dadi = "sulochna")
+
+def my_function(child1, child2, child3, child4, mummy, papa, dadi):
+    print("my name is: " + child1)
+
+my_function(child1 = "tanishq", child2 = "daksh", child3 = "diwakar", child4 = "swastik", mummy = "sangeeta", papa = "pankaj", dadi = "sulochna")
+
+def my_function(child1, child2, child3, child4, mummy, papa, dadi):
+    print("this is my brother name: " + child4)
+
+my_function(child1 = "tanishq", child2 = "daksh", child3 = "diwakar", child4 = "swastik", mummy = "sangeeta", papa = "pankaj", dadi = "sulochna")
+
+def my_function(child1, child2, child3, child4, mummy, papa, dadi):
+    print("this is my mummy's name: " + mummy)
+
+my_function(child1 = "tanishq", child2 = "daksh", child3 = "diwakar", child4 = "swastik", mummy = "sangeeta", papa = "pankaj", dadi = "sulochna")
+
+def my_function(child1, child2, child3, child4, mummy, papa, dadi):
+    print("this is my father's name: " + papa)
+
+my_function(child1 = "tanishq", child2 = "daksh", child3 = "diwakar", child4 = "swastik", mummy = "sangeeta", papa = "pankaj", dadi = "sulochna")
+
+def my_function(child1, child2, child3, child4, mummy, papa, dadi):
+    print("this is my dadi's name: " + dadi)
+
+my_function(child1 = "tanishq", child2 = "daksh", child3 = "diwakar", child4 = "swastik", mummy = "sangeeta", papa = "pankaj", dadi = "sulochna")
